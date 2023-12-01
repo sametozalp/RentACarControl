cfa8e1f45f5d56cd081d22ed5e406691c657f7ce
Samet Özalp <93615071+sametozalp@users.noreply.github.com>
2023-12-01 14:52:02 +0300

    Initial commit

---
+++ RentACarControl/Program.cs
@@ -0,0 +1,41 @@
+﻿using System.Data;
+using System.Data.SqlClient;
+
+namespace RentACarControl
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            String query = "SELECT * FROM OGRENCI";
+            SqlConnection sqlConnection = new SqlConnection("Data Source=DESKTOP-5HFM0UD;Initial Catalog=DATABASE;Integrated Security=True");
+            
+            sqlConnection.Open();
+
+            SqlCommand sqlCommand = new SqlCommand(query, sqlConnection);
+            Console.WriteLine(sqlCommand.CommandText);
+        
+            SqlDataAdapter adapter = new SqlDataAdapter(sqlCommand);
+            DataTable dataTable = new DataTable();
+            adapter.Fill(dataTable);
+
+            List<string> list = new List<string>();
+            foreach (DataRow row in dataTable.Rows)
+            {
+                foreach (DataColumn column in dataTable.Columns)
+                {
+                    list.Add($"{column.ColumnName}: {row[column]}");
+                }
+            }
+
+            // Listeyi düzgün bir şekilde yazdırma
+            foreach (var item in list)
+            {
+                Console.WriteLine(item);
+            }
+
+            sqlConnection.Close();
+
+        }
+    }
+}
