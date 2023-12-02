83d4409759137048f3ebb584353388fc58024f5d
Samet Özalp <93615071+sametozalp@users.noreply.github.com>
2023-12-02 15:04:29 +0300

    added RentACarDatabaseConnection class

--- RentACarControl/Program.cs
+++ RentACarControl/Program.cs
@@ -7,39 +7,15 @@ namespace RentACarControl
     {
         static void Main(string[] args)
         {
-            string query = "SELECT * FROM BAKIM";
-            SqlConnection sqlConnection = new SqlConnection("Data Source=DESKTOP-5HFM0UD;Initial Catalog=DATABASE;Integrated Security=True");
+           RentACarDatabaseConnection databaseConnection = new RentACarDatabaseConnection("Data Source=DESKTOP-5HFM0UD;Initial Catalog=DATABASE;Integrated Security=True");
             
-            sqlConnection.Open();
+            databaseConnection.open();
 
-            SqlCommand sqlCommand = new SqlCommand(query, sqlConnection);
-            Console.WriteLine(sqlCommand.CommandText);
-        
-            SqlDataAdapter adapter = new SqlDataAdapter(sqlCommand);
-            DataTable dataTable = new DataTable();
-            adapter.Fill(dataTable);
-
-            DateTime dateTimeNow = DateTime.Now;
-
-            List<User> list = new List<User>();
-            User user;
-            foreach (DataRow row in dataTable.Rows) 
-            {
-                user = new User(Convert.ToInt32(row["id"]), row["mail"].ToString(), Convert.ToDateTime(row["date"]));
-                list.Add(user);
-            }
-            int subDays;
-            foreach (var item in list)
-            {
-                Console.WriteLine("id: " + item.getAd() + " date: " + item.getDate() + " mail: " + item.getMail());
-                subDays = (item.getDate() - dateTimeNow).Days + 1;
-                Console.WriteLine(subDays + " gün fark..");
-
-            }
-
-
-            sqlConnection.Close();
-
+            string query = "SELECT * FROM BAKIM";
+            
+            databaseConnection.getQueryData(query);
+            
+            databaseConnection.close();
         }
     }
 }
---
+++ RentACarControl/RentACarDatabaseConnection.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+
+namespace RentACarControl
+{
+    internal class RentACarDatabaseConnection
+    {
+        public RentACarDatabaseConnection(string databaseString)
+        {
+            try
+            {
+                sqlConnection = new SqlConnection(databaseString);
+            }catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+        public void open()
+        {
+            sqlConnection.Open();
+        }
+
+        public void getQueryData(string query)
+        {
+            try
+            {
+                sqlCommand = new SqlCommand(query, sqlConnection);
+
+                adapter = new SqlDataAdapter(sqlCommand);
+                dataTable = new DataTable();
+                adapter.Fill(dataTable);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            
+            DateTime dateTimeNow = DateTime.Now.Date;
+
+            User user;
+            foreach (DataRow row in dataTable.Rows)
+            {
+                user = new User(Convert.ToInt32(row["id"]), row["mail"].ToString(), Convert.ToDateTime(row["date"]));
+                list.Add(user);
+            }
+            bool subDays;
+
+            foreach (var item in list)
+            {
+                Console.WriteLine("id: " + item.getAd() + " date: " + item.getDate() + " mail: " + item.getMail());
+                subDays = item.getDate() == dateTimeNow;
+                Console.WriteLine(subDays);
+            }
+        }
+
+        public void close()
+        {
+            try
+            {
+                sqlConnection.Close()
+            }
+            catch (Exception ex) { 
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+        SqlConnection sqlConnection;
+        SqlCommand sqlCommand;
+        SqlDataAdapter adapter;
+        DataTable dataTable;
+        public List<User> list = new List<User>();
+    }
+}
